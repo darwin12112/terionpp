5c903263a33304dddfe20b9c4e675711d20fae54
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-10-09 20:49:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/response', async function (req, res) {
           expiresIn: "1h",
         }
       );
-      return res.redirect("https://www.mantrimall.net/api/response-recharge/" + token);
+      return res.redirect("https://www.terion.club/api/response-recharge/" + token);
     }
     
     res.redirect("/");
