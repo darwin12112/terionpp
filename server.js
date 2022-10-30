4effd31286f7ebb3763ba2b09417fd6c14b78a2e
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-10-30 15:42:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/response', async function (req, res) {
           expiresIn: "1h",
         }
       );
-      return res.redirect("https://www.booe.in/api/response-recharge/" + token);
+      return res.redirect("https://www.wingotrading.in/api/response-recharge/" + token);
     }
     
     res.redirect("/");
