33763d17c41401119360b5f7f9894e3712e0f413
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2022-11-04 11:43:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -223,7 +223,7 @@ class App extends React.Component {
             </div>
           </div>
         </main>
-        <footer><a href="/about_us.htm" target="about"><b> About Us </b> </a> &nbsp; &nbsp; &nbsp;
+        <footer><a href="/about_us.htm" target="about"><strong> About Us </strong> </a> &nbsp; &nbsp; &nbsp;
         <a href="/contact_us.htm" target="contact_us">Contact US</a> &nbsp; &nbsp; &nbsp;
 		 <a href="/shipping_Policy.htm" target="shipping_Policy">Shipping Policy</a> &nbsp; &nbsp; &nbsp;
         <a href="/privacy_Policy.htm" target="privacy_Policy">Privacy Policy</a> &nbsp; &nbsp; &nbsp;
