a34c25a0acf1b96f2f4b24498686a2f249ecf176
darwin12112 <70789419+darwin12112@users.noreply.github.com>
2023-07-21 17:59:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post('/response', async function (req, res) {
           expiresIn: "1h",
         }
       );
-      return res.redirect("https://bingoclub.in/api/response-recharge/" + token);
+      return res.redirect("https://gamesbet.in/api/response-recharge/" + token);
     }
     
     res.redirect("/");
